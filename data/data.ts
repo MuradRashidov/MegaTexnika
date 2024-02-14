a5616c8ae3568cc75555c1778b9d64351f3d5d4d
MuradRashidov <muradrashidov995@gmail.com>
2024-02-14 23:07:49 +0400

    dataid

--- data/data.ts
+++ data/data.ts
@@ -2,7 +2,7 @@ import ITechnique from "@/interfaces/data";
 
 const techniques:ITechnique [] = [
     {
-        id:1,
+        _id:"1",
         name:"Ekskavator",
         categoryName:"Ekskavatorlar",
         imageUrl:"/images/home/ekskavator.png",
@@ -11,7 +11,7 @@ const techniques:ITechnique [] = [
         productionYear:2022
     },
     {
-        id:2,
+        _id:"2",
         name:"Ekskavator yüklıyici",
         categoryName:"Ekskavator yükləyici",
         imageUrl:"/images/home/ekskavatoryukleyici.png",
@@ -20,7 +20,7 @@ const techniques:ITechnique [] = [
         productionYear:2022
     },
     {
-        id:3,
+        _id:"3",
         name:"Avtokran",
         categoryName:"Avtokranlar",
         imageUrl:"/images/home/avtokran.png",
@@ -29,7 +29,7 @@ const techniques:ITechnique [] = [
         productionYear:2022
     },
     {
-        id:4,
+        _id:"4",
         name:"Forklift",
         categoryName:"Forkliftlər",
         imageUrl:"/images/home/forklift.png",
@@ -38,7 +38,7 @@ const techniques:ITechnique [] = [
         productionYear:2022
     },
     {
-        id:5,
+        _id:"5",
         name:"Katok",
         categoryName:"Katok",
         imageUrl:"/images/home/katok.png",
@@ -47,7 +47,7 @@ const techniques:ITechnique [] = [
         productionYear:2022
     },
     {
-        id:6,
+        _id:"6",
         name:"Shacman",
         categoryName:"Shacman",
         imageUrl:"/images/home/shacman.png",
@@ -56,7 +56,7 @@ const techniques:ITechnique [] = [
         productionYear:2022
     },
     {
-        id:7,
+        _id:"7",
         name:"Bobcat",
         categoryName:"Bobcat",
         imageUrl:"/images/home/bobcat.png",
@@ -65,7 +65,7 @@ const techniques:ITechnique [] = [
         productionYear:2022
     },
     {
-        id:8,
+        _id:"8",
         name:"Pompa",
         categoryName:"Pompa",
         imageUrl:"/images/home/pompa.png",
