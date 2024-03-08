d6e6a2f0ef840f43ed28b185c250966fa50ac1c6
MuradRashidov <muradrashidov995@gmail.com>
2024-03-08 21:50:11 +0400

    adminresponsivexsdsfjdd

--- middleware.ts
+++ middleware.ts
@@ -1,41 +1,41 @@
-import { getToken } from 'next-auth/jwt';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+// import { getToken } from 'next-auth/jwt';
+// import { NextResponse } from 'next/server';
+// import type { NextRequest } from 'next/server';
 
-export async function middleware(request: NextRequest) {
-    const path = request.nextUrl.pathname;
-    const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
-    console.log("token",token)
-    const publicPath = path === "/login" ||path === "/register";
-    const adminPath = path === "/createtechnique" || path === "/orders" || path === "/admintechniques";
+// export async function middleware(request: NextRequest) {
+//     const path = request.nextUrl.pathname;
+//     const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+//     console.log("token",token)
+//     const publicPath = path === "/login" ||path === "/register";
+//     const adminPath = path === "/createtechnique" || path === "/orders" || path === "/admintechniques";
    
 
     
-    if (publicPath && token) {
-            return NextResponse.redirect(new URL("/",request.nextUrl));
+//     if (publicPath && token) {
+//             return NextResponse.redirect(new URL("/",request.nextUrl));
         
-    }
-    if (!publicPath && !token) {
-        return NextResponse.redirect(new URL("/register",request.nextUrl));
-    }
-    if(adminPath && token?.role !== "admin"){
-        return NextResponse.redirect(new URL("/",request.nextUrl));
+//     }
+//     if (!publicPath && !token) {
+//         return NextResponse.redirect(new URL("/register",request.nextUrl));
+//     }
+//     if(adminPath && token?.role !== "admin"){
+//         return NextResponse.redirect(new URL("/",request.nextUrl));
 
-    }
-   else if(!adminPath && !publicPath && token?.role !== "user"){
-        return NextResponse.redirect(new URL("/createtechnique",request.nextUrl));
+//     }
+//    else if(!adminPath && !publicPath && token?.role !== "user"){
+//         return NextResponse.redirect(new URL("/createtechnique",request.nextUrl));
 
-    }
-    else{
-        return NextResponse.redirect(new URL("/",request.nextUrl));
+//     }
+//     else{
+//         return NextResponse.redirect(new URL("/",request.nextUrl));
  
-    }
+//     }
 
-}
+// }
 
-export const config = {
+// export const config = {
     
-    matcher: [
-        "/login","/register","/","/about","/home","/techniques","/createtechnique","/orders","/admintechniques"
-    ]
-};
+//     matcher: [
+//         "/login","/register","/","/about","/home","/techniques","/createtechnique","/orders","/admintechniques"
+//     ]
+// };
