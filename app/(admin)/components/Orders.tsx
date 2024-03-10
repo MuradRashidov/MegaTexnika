25e6a96de3be7681d94317fd9f4dcb62818d7eb6
MuradRashidov <muradrashidov995@gmail.com>
2024-03-10 07:44:09 +0400

    correctingggdh

--- app/(admin)/components/Orders.tsx
+++ app/(admin)/components/Orders.tsx
@@ -1,4 +1,3 @@
-"use client"
 import { Container } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
@@ -16,7 +15,6 @@ const StyledTableCell = styled(TableCell)(({ theme }:any) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: "#FFC01F",
     color: theme.palette.common.white,
-    
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
@@ -29,45 +27,48 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
     backgroundColor: theme.palette.action.hover,
   },
-  // hide last border
   '&:last-child td, &:last-child th': {
     border: 0,
   },
 }));
 
 const Orders = () => {
-    const [orders,setOrders] = useState([]);
-    useEffect(()=>{
-        axios.get("/api/orders")
-        .then(({data})=>setOrders(data))
+    const [orders, setOrders] = useState([]);
 
-    },[])
-    console.log(orders)
-  return (
-    <Container>
-                <TableContainer sx={{ maxWidth: {xs:500, md:900,lg:1100 } }} component={Paper}>
-          <Table sx={{ minWidth: 500,maxWidth:{xs:500,md:900,lg:1100} }} aria-label="customized table">
-            <TableHead>
-              <TableRow>
-                <StyledTableCell align="left" >Məhsul adı</StyledTableCell>
-                <StyledTableCell align="left" >Məhsul rəsmi</StyledTableCell>
-                <StyledTableCell align="left" >Sifarişçi adı</StyledTableCell>
-                <StyledTableCell align="left">Şirkət adı</StyledTableCell>
-                <StyledTableCell align="left">Email</StyledTableCell>
-                <StyledTableCell align="left">Telefon</StyledTableCell>
-                <StyledTableCell align="left">Sifariş məbləği</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody sx={{ minWidth: 500,maxWidth:{xs:500,md:900,lg:1100} }}>
-            {orders.map((order:any,index:number)=>{
-            return<Order key={index} {...order}/>
-            })} 
-            </TableBody>
-          </Table>
-        </TableContainer>
-        
-    </Container>
-  )
+    const fetchOrders = () => {
+        axios.get("/api/orders")
+            .then(({ data }) => setOrders(data))
+            .catch(error => console.error('Error fetching orders:', error));
+    };
+
+    useEffect(() => {
+        fetchOrders();
+    }, []); // Boş bağımlılık dizisi, yalnızca bileşen ilk render edildiğinde çalışmasını sağlar
+
+    return (
+        <Container>
+            <TableContainer sx={{ maxWidth: { xs: 500, md: 900, lg: 1100 } }} component={Paper}>
+                <Table sx={{ minWidth: 500, maxWidth: { xs: 500, md: 900, lg: 1100 } }} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell align="left" >Məhsul adı</StyledTableCell>
+                            <StyledTableCell align="left" >Məhsul rəsmi</StyledTableCell>
+                            <StyledTableCell align="left" >Sifarişçi adı</StyledTableCell>
+                            <StyledTableCell align="left">Şirkət adı</StyledTableCell>
+                            <StyledTableCell align="left">Email</StyledTableCell>
+                            <StyledTableCell align="left">Telefon</StyledTableCell>
+                            <StyledTableCell align="left">Sifariş məbləği</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody sx={{ minWidth: 500, maxWidth: { xs: 500, md: 900, lg: 1100 } }}>
+                        {orders.map((order: any, index: number) => {
+                            return <Order key={index} {...order} />
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Container>
+    );
 }
 
-export default Orders
+export default Orders;
